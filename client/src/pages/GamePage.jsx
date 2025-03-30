9d47d3fcf51c2ec5e123ed0a86eb881c993995e0
Uriel Niazov <urielniazov@gmail.com>
2025-03-30 10:23:12 +0300

    updated ui on error

--- client/src/pages/GamePage.jsx
+++ client/src/pages/GamePage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useGame } from '../context/GameContext';
 import Header from '../components/Header';
 import PuzzleDisplay from '../components/PuzzleDisplay';
 import GuessForm from '../components/GuessForm';
@@ -7,15 +8,22 @@ import GameOverPanel from '../components/GameOverPanel';
 import InstructionsPanel from '../components/InstructionsPanel';
 
 const GamePage = () => {
+  const { isLoading, error } = useGame();
+
   return (
     <div className="flex flex-col items-center min-h-screen bg-gray-50 p-4">
       <Header />
       <main className="w-full max-w-md">
         <div className="bg-white rounded-lg shadow-md p-6 mb-6">
           <PuzzleDisplay />
-          <GuessForm />
-          <AttemptsList />
-          <GameOverPanel />
+          
+          {!error && !isLoading && (
+            <>
+              <GuessForm />
+              <AttemptsList />
+              <GameOverPanel />
+            </>
+          )}
         </div>
         <InstructionsPanel />
       </main>
